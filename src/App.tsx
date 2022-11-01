43d1f16c272b1247fdb466ebfadb86b925109b7b
Hecedu <hector.maganahdez@gmail.com>
2022-11-01 17:21:02 -0600

    Visual tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { ChangeEventHandler, useState } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import axios from 'axios';
 
@@ -17,22 +16,22 @@ function App() {
     var formattedDateQuery = dateQuery.replace('-', '')
 
     await axios.get(`https://archive.org/wayback/available?url=https://${searchQuery}&timestamp=${formattedDateQuery}`)
-    .then((result) => {
-      if (result.data.archived_snapshots.closest.url != undefined) {
-        var url = result.data.archived_snapshots.closest.url as string
-        var position = 41
-        var output = url.substring(0, position) + "if_" + url.substring(position);
-        window.location.href = output
-      }
-      else {
+      .then((result) => {
+        if (result.data.archived_snapshots.closest.url !== undefined) {
+          var url = result.data.archived_snapshots.closest.url as string
+          var position = 41
+          var output = url.substring(0, position) + "if_" + url.substring(position);
+          window.location.href = output
+        }
+        else {
+          setIsSearching(false)
+          setError('Sadly no captures of that website where found')
+        }
+      })
+      .catch(() => {
         setIsSearching(false)
-        setError('Sadly no captures of that website where found')
-      }
-    })
-    .catch(()=> {
-      setIsSearching(false)
-      setError('An Unexpected Error Ocurred')
-    })
+        setError('An Unexpected Error Ocurred')
+      })
   }
   const searchChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
     setSearchQuery(e.currentTarget.value);
@@ -53,23 +52,39 @@ function App() {
         isSearching ?
           <div className='container text-center vh-100 p-5 d-flex align-items-center justify-content-center'>
             <div>
-              <h1>One of our pigs is sniffing your website...</h1>
-              <img src={require('./Assets/pig_eating_md_clr.gif')}></img>
+              <h5>One of our pigs is sniffing your website...</h5>
+              <img src={require('./Assets/pig_eating_md_clr.gif')} alt="cartoon pig eating wheat"/>
             </div>
           </div> :
           <div className='container text-center vh-100 p-5 d-flex align-items-center justify-content-center'>
             <div>
-              <h1 className='display-2' style={{ textShadow: `${isThemeDark ? 'yellow' : 'black'} 0px 5px 8px` }}>Truffle!</h1>
-              <p>Dig your favorite websites...</p>
+              {
+                isThemeDark ? <img src={require('./Assets/truffle_logo_light.png')} alt="truffle logo"/> : <img src={require('./Assets/truffle_logo.png')} alt="truffle logo"/>
+              }
+              <p>Dig the past of your favorite websites...</p>
               <form onSubmit={getWebsite}>
-                <input value={searchQuery} onChange={searchChangeHandler}></input>
+                <input value={searchQuery} onChange={searchChangeHandler} placeholder={"www.example.com"}></input>
                 <button onClick={getWebsite}>Dig!</button>
               </form>
-              <input className="my-1" type="date" value={dateQuery} onChange={dateChangeHandler} />
-              <p className='mt-2 mb-0'>©2022 B-Llage Digital Media</p>
-              <small>This project is possible thanks to the <a href='https://archive.org/web/'>Wayback Machine</a> and the <a href='https://archive.org/'>Internet Archive</a>.</small>
-              <p></p>
+              <input className="my-1" type="date" min={'1990-01-01'} value={dateQuery} onChange={dateChangeHandler} />
+              <p><small style={{color: "green"}}>Note: Truffle! is not a search engine. Please input domains in the www.example.com format.</small></p>
               <button onClick={() => { setIsThemeDark(!isThemeDark) }}>☼</button>
+              {
+                error ? <p style={{ color: 'red' }}>{error}</p> : <></>
+              }
+              <hr></hr>
+              <div className='d-flex'>
+                <div>
+                  <img className='img-fluid' src={require('./Assets/bllage.gif')} alt="B Llage logo continuously spinning"/>
+                </div>
+                <div className='text-start'>
+                  <p className='mt-2 mb-0'>©2022 B-Llage Digital Media</p>
+                  <p className='m-0'><small>This project is possible thanks to the <a href='https://archive.org/web/'>Wayback Machine</a> and the <a href='https://archive.org/'>Internet Archive</a>.</small></p>
+                  <p><small>See more projects by me: <a href='https://hectormagana.art'>Héctor Magaña</a></small></p>
+                </div>
+                <img src={require('./Assets/pigflying.gif')} alt="Pigs flying with the word 'home' attached"/>
+              </div>
+
             </div>
           </div>
       }
